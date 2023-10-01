f9b2c6e28fbfbd2981aac0f5aff89873652f082d
Norgius <dmitry.schadrin21@yandex.ru>
2023-10-01 15:42:07 +0300

    Initial commit

---
+++ users/schemas.py
@@ -0,0 +1,9 @@
+from typing import Annotated
+
+from pydantic import BaseModel, EmailStr, Field
+
+
+class CreateUser(BaseModel):
+    # username: str = Field(..., min_length=3, max_length=20)
+    username: Annotated[str, Field(min_length=3, max_length=20)]
+    email: EmailStr
