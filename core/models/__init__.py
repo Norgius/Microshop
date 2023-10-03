a2d4899072658211d536100d4da6a2e831560721
Norgius <dmitry.schadrin21@yandex.ru>
2023-10-03 22:43:16 +0300

    Create posts model

--- core/models/__init__.py
+++ core/models/__init__.py
@@ -1,6 +1,7 @@
 __all__ = {
     "Base",
     "User",
+    "Post",
     "Product",
     "DataBaseHelper",
     "db_helper",
@@ -9,5 +10,6 @@ __all__ = {
 
 from .base import Base
 from .product import Product
+from .post import Post
 from .db_helper import DataBaseHelper, db_helper
 from .user import User
