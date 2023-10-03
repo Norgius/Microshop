656b30053d5749cef43c45827ac52c950ba972e2
Norgius <dmitry.schadrin21@yandex.ru>
2023-10-03 12:09:44 +0300

    Add import for packet of models

--- core/models/__init__.py
+++ core/models/__init__.py
@@ -1,10 +1,11 @@
-from .base import Base
-from .product import Product
-from .db_helper import DataBaseHelper, db_helper
-
 __all__ = {
     "Base",
     "Product",
     "DataBaseHelper",
     "db_helper",
 }
+
+
+from .base import Base
+from .product import Product
+from .db_helper import DataBaseHelper, db_helper
