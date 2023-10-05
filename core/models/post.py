67ceaa71dfd5612d1139c5aeaf359c93cd8d5baa
Norgius <dmitry.schadrin21@yandex.ru>
2023-10-05 16:25:52 +0300

    Update after create mixin

--- core/models/post.py
+++ core/models/post.py
@@ -1,15 +1,12 @@
-from typing import TYPE_CHECKING
-
-from sqlalchemy import String, Text, ForeignKey
-from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy import String, Text
+from sqlalchemy.orm import Mapped, mapped_column
 
 from .base import Base
-
-if TYPE_CHECKING:
-    from .user import User
+from .mixins import UserRelationMixin
 
 
-class Post(Base):
+class Post(UserRelationMixin, Base):
+    _user_back_populates = "posts"
 
     id: Mapped[int] = mapped_column(primary_key=True)
     title: Mapped[str] = mapped_column(String(100))
@@ -18,7 +15,3 @@ class Post(Base):
         default="",
         server_default="",
     )
-    user_id: Mapped[int] = mapped_column(
-        ForeignKey("users.id"),
-    )
-    user: Mapped["User"] = relationship(back_populates="posts")
