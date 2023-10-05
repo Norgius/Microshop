27e8d9a4ca7b342993f5d47cf782f46bb87fd709
Norgius <dmitry.schadrin21@yandex.ru>
2023-10-05 16:24:44 +0300

    Create Profile table for User'

--- core/models/__init__.py
+++ core/models/__init__.py
@@ -2,6 +2,7 @@ __all__ = {
     "Base",
     "User",
     "Post",
+    "Profile",
     "Product",
     "DataBaseHelper",
     "db_helper",
@@ -10,6 +11,7 @@ __all__ = {
 
 from .base import Base
 from .product import Product
-from .post import Post
 from .db_helper import DataBaseHelper, db_helper
 from .user import User
+from .post import Post
+from .profile import Profile
--- core/models/user.py
+++ core/models/user.py
@@ -7,10 +7,12 @@ from .base import Base
 
 if TYPE_CHECKING:
     from .post import Post
+    from .profile import Profile
 
 
 class User(Base):
 
     id: Mapped[int] = mapped_column(primary_key=True)
     username: Mapped[str] = mapped_column(String(50), unique=True)
-    posts: Mapped[list["Post"]] = relationship(back_populates="users")
+    posts: Mapped[list["Post"]] = relationship(back_populates="user")
+    profile: Mapped["Profile"] = relationship(back_populates="user")
