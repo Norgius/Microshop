99b822b11d36fb3afb620f05327f9d64ef470d74
Norgius <dmitry.schadrin21@yandex.ru>
2023-11-29 14:42:20 +0300

    Add new crud commands for many-to-many relationships

--- crud.py
+++ crud.py
@@ -5,7 +5,7 @@ from sqlalchemy.engine import Result
 from sqlalchemy.orm import joinedload, selectinload
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from core.models import db_helper, User, Profile, Post
+from core.models import db_helper, User, Profile, Post, Order, Product, OrderProductAssociation
 
 
 async def create_user(session: AsyncSession, username: str) -> User:
@@ -135,51 +135,203 @@ async def create_user_profile(
     return profile
 
 
+async def main_relations(session: AsyncSession):
+    await create_user(session=session, username='join')
+    await create_user(session=session, username='sam')
+    await create_user(session=session, username='alice')
+
+    user_sam = await get_user_by_username(session=session, username="sam")
+    user_john = await get_user_by_username(session=session, username="join")
+    await get_user_by_username(session=session, username="sam")
+    await get_user_by_username(session=session, username="bob")
+
+    if user_sam and user_john:
+        await create_user_profile(
+            session=session,
+            user_id=user_sam.id,
+            first_name="Сэм",
+            last_name="Егоров"
+        )
+        await create_user_profile(
+            session=session,
+            user_id=user_john.id,
+            first_name="Джон"
+        )
+
+    users = await show_users_with_profiles(session)
+    print(users)
+
+    if user_sam and user_john:
+        john_posts = await create_posts(
+            session,
+            user_john.id,
+            'SQL SELECT',
+            'SQL JOIN',
+        )
+        sam_posts = await create_posts(
+            session,
+            user_sam.id,
+            'SQL WHERE',
+            'SQL FROM',
+        )
+        print(john_posts)
+        print(sam_posts)
+
+    await get_users_with_posts(session)
+    await get_post_with_user(session)
+    await get_users_with_posts_and_profiles(session)
+    await get_profile_with_users_and_users_with_posts(session)
+
+
+async def create_order(session: AsyncSession, promocode: str | None = None) -> Order:
+    order = Order(promocode=promocode)
+
+    session.add(order)
+    await session.commit()
+
+    return order
+
+
+async def create_product(
+        session: AsyncSession,
+        name: str,
+        description: str,
+        price: int,
+) -> Product:
+    product = Product(
+        name=name,
+        description=description,
+        price=price,
+    )
+
+    session.add(product)
+    await session.commit()
+    return product
+
+
+async def create_orders_and_products(session: AsyncSession):
+    order_one = await create_order(session)
+    order_promo = await create_order(session, promocode='promo')
+
+    mouse = await create_product(
+        session,
+        'Mouse',
+        'Great gaming mouse',
+        120,
+    )
+    keyboard = await create_product(
+        session,
+        'Keyboard',
+        'Great gaming keyboard',
+        149,
+    )
+    display = await create_product(
+        session,
+        'Display',
+        'Office display',
+        249,
+    )
+
+    order_one = await session.scalar(
+        select(Order)
+        .where(Order.id == order_one.id)
+        .options(
+            selectinload(Order.products),
+        ),
+    )
+    order_promo = await session.scalar(
+        select(Order)
+        .where(Order.id == order_promo.id)
+        .options(
+            selectinload(Order.products),
+        ),
+    )
+
+    order_one.products.append(mouse)
+    order_one.products.append(keyboard)
+    # order_promo.products.append(keyboard)
+    # order_promo.products.append(display)
+    order_promo.products = [keyboard, display]
+
+    await session.commit()
+
+
+async def get_orders_with_products_secondary(sesstion: AsyncSession) -> list[Order]:
+    stmt = (
+        select(Order)
+        .options(
+            selectinload(Order.products),
+        )
+        .order_by(Order.id)
+    )
+    orders = await sesstion.scalars(stmt)
+    return list(orders)
+
+
+async def demo_get_orders_with_products_through_secondary(session: AsyncSession):
+
+    orders = await get_orders_with_products_secondary(session)
+    for order in orders:
+        print(order.id, order.promocode, order.created_at, 'products:')
+        for product in order.products:
+            print('-', product.id, product.name, product.price)
+
+
+async def get_orders_with_products_with_association(sesstion: AsyncSession) -> list[Order]:
+    stmt = (
+        select(Order)
+        .options(
+            selectinload(Order.products_details).joinedload(
+                OrderProductAssociation.product
+            ),
+        )
+        .order_by(Order.id)
+    )
+    orders = await sesstion.scalars(stmt)
+    return list(orders)
+
+
+async def demo_get_orders_with_products_with_association(session: AsyncSession):
+    orders = await get_orders_with_products_with_association(session)
+
+    for order in orders:
+        print(order.id, order.promocode, order.created_at, 'products:')
+        for order_product_detail in order.products_details:
+            print('-', order_product_detail.product.id, order_product_detail.product.name,
+                  order_product_detail.product.price, 'quantity:', order_product_detail.count)
+
+
+async def create_gift_product_for_existing_orders(session: AsyncSession):
+    orders = await get_orders_with_products_with_association(session)
+    gift_product = await create_product(
+        session,
+        name='Gift',
+        description='Gift for you',
+        price=0,
+    )
+    for order in orders:
+        order.products_details.append(
+            OrderProductAssociation(
+                count=1,
+                unit_price=0,
+                product=gift_product,
+            )
+        )
+
+    await session.commit()
+
+
+async def demo_m2m(session: AsyncSession):
+    # await create_orders_and_products(session)
+    # await demo_get_orders_with_products_through_secondary(session)
+    await demo_get_orders_with_products_with_association(session)
+    # await create_gift_product_for_existing_orders(session)
+
+
 async def main():
     async with db_helper.session_factory() as session:
-        # await create_user(session=session, username='join')
-        # await create_user(session=session, username='sam')
-        # await create_user(session=session, username='alice')
-
-        # user_sam = await get_user_by_username(session=session, username="sam")
-        # user_john = await get_user_by_username(session=session, username="join")
-        # await get_user_by_username(session=session, username="sam")
-        # await get_user_by_username(session=session, username="bob")
-
-        # await create_user_profile(
-        #     session=session,
-        #     user_id=user_sam.id,
-        #     first_name="Сэм",
-        #     last_name="Егоров"
-        # )
-        # await create_user_profile(
-        #     session=session,
-        #     user_id=user_john.id,
-        #     first_name="Джон"
-        # )
-
-        # users = await show_users_with_profiles(session)
-        # print(users)
-
-        # john_posts = await create_posts(
-        #     session,
-        #     user_john.id,
-        #     'SQL SELECT',
-        #     'SQL JOIN',
-        # )
-        # sam_posts = await create_posts(
-        #     session,
-        #     user_sam.id,
-        #     'SQL WHERE',
-        #     'SQL FROM',
-        # )
-        # print(john_posts)
-        # print(sam_posts)
-
-        # await get_users_with_posts(session)
-        # await get_post_with_user(session)
-        # await get_users_with_posts_and_profiles(session)
-        await get_profile_with_users_and_users_with_posts(session)
+        # await main_relations(session)
+        await demo_m2m(session)
 
 
 if __name__ == "__main__":
