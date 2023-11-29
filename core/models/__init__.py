276ebb2b496204a003e991bbf948dc8638b8d5d5
Norgius <dmitry.schadrin21@yandex.ru>
2023-11-29 14:40:54 +0300

    Create many-to-many relationships between product and order

--- core/models/__init__.py
+++ core/models/__init__.py
@@ -2,10 +2,12 @@ __all__ = (
     "Base",
     "User",
     "Post",
+    "Order",
     "Profile",
     "Product",
     "DataBaseHelper",
     "db_helper",
+    "OrderProductAssociation",
 )
 
 from .base import Base
@@ -14,3 +16,5 @@ from .db_helper import DataBaseHelper, db_helper
 from .user import User
 from .post import Post
 from .profile import Profile
+from .order import Order
+from .order_product_association import OrderProductAssociation
--- core/models/product.py
+++ core/models/product.py
@@ -1,7 +1,13 @@
-from sqlalchemy.orm import Mapped
-from sqlalchemy.orm import mapped_column
+from __future__ import annotations
+from typing import TYPE_CHECKING
+
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from .base import Base
+
+if TYPE_CHECKING:
+    from .order import Order
+    from .order_product_association import OrderProductAssociation
 
 
 class Product(Base):
@@ -10,3 +16,13 @@ class Product(Base):
     name: Mapped[str]
     description: Mapped[str]
     price: Mapped[int]
+
+    orders: Mapped[list[Order]] = relationship(
+        secondary='order_product_association',
+        back_populates='products',
+    )
+
+    # association between Order -> Association -> Product
+    orders_details: Mapped[list[OrderProductAssociation]] = relationship(
+        back_populates='product'
+    )
